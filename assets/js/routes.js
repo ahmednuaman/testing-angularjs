2b7b5a7c5daac91ab4589f91902b4f74263b962f
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-06-06 19:06:40 +0100

    added our routes file that bootstraps the app

---
+++ assets/js/routes.js
@@ -0,0 +1,17 @@
+define([
+  'config',
+  'angular',
+  'controller/app-controller',
+  'controller/menu-controller'
+], function(cfg, A) {
+  var routes = function($routeProvider, $locationProvider) {
+    // $routeProvider.when('/home', {
+    //   templateUrl: cfg.paths.partial + 'home-partial.html'
+    // });
+  };
+
+  var app = A.module(cfg.ngApp);
+  app.config(routes);
+
+  A.bootstrap(document, [cfg.ngApp]);
+});
