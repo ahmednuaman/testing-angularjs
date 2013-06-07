0414a6153000dee21da9b0bbb59a494d30386993
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-06-07 12:09:28 +0100

    removed requirejs out of here so this can be used in tests too

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,17 +1,4 @@
-require.config({
-  baseUrl: 'assets/js/',
-  urlArgs: 'x=' +  (new Date()).getTime(),
-  paths: {
-    'angular': '../vendor/angular/angular'
-  },
-  shim: {
-    'angular': {
-      exports: 'angular'
-    }
-  }
-});
-
-define([], function () {
+define([], function() {
   return {
     ngApp: 'testing-angularjs'
   };
