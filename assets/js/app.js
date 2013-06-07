8abbeee9486878c379c2ad7f6456fe4d8e17ed6c
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-06-07 12:09:16 +0100

    moved requirejs config into here

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,18 @@
-require(['config'], function(cfg) {
-  require(['angular'], function(A) {
-    A.module(cfg.ngApp, []);
+require.config({
+  baseUrl: 'assets/js/',
+  urlArgs: 'x=' +  (new Date()).getTime(),
+  paths: {
+    'angular': '../vendor/angular/angular'
+  },
+  shim: {
+    'angular': {
+      exports: 'angular'
+    }
+  }
+});
 
-    require(['routes']);
-  });
+require(['config', 'angular'], function(cfg, A) {
+  A.module(cfg.ngApp, []);
+
+  require(['routes']);
 });
