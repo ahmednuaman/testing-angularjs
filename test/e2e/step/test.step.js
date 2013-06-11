6188b63b8ce508177a7730a93e24864d669bfbec
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-06-11 09:43:46 +0100

    added another test

--- test/e2e/step/test.step.js
+++ test/e2e/step/test.step.js
@@ -5,5 +5,6 @@ describe('The app', function() {
 
   it('should load a menu', function() {
     expect(element('.menu-item').count()).toBeGreaterThan(0);
+    expect(element('#menu').count()).toBe(1);
   });
 });
