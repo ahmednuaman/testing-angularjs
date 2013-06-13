c14f373871fbb6ad7db4197b781cf0b5f4dc6084
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-06-13 13:18:25 +0100

    added partial and route testing

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -6,9 +6,16 @@ define([
   'controller/menu-controller'
 ], function(cfg, A) {
   var routes = function($routeProvider, $locationProvider) {
-    // $routeProvider.when('/home', {
-    //   templateUrl: cfg.paths.partial + 'home-partial.html'
-    // });
+    $routeProvider.when('/home', {
+        templateUrl: '/assets/partial/home-partial.html'
+      })
+      .otherwise({
+        redirectTo: '/home'
+      });
+
+    $locationProvider
+      .html5Mode(true)
+      .hashPrefix('!');
   };
 
   var app = A.module(cfg.ngApp);
